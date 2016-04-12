36f5fb7b78bb743ea4bde54c828ad096941584c1
Augustin Borsu <augustin@sagacify.com>
2016-04-12 17:46:14 +0200

    fix(status_rules): Ignore merge commits

--- komitet_git_bezopasnosti/rules/status_rules.py
+++ komitet_git_bezopasnosti/rules/status_rules.py
@@ -1,6 +1,9 @@
 from .. import MAX_STATUS_LENGTH
 from .. import STATUS_R
 
+def _is_merge(status_line):
+    return status_line.startswith("Merge pull request #")
+
 
 def check_status_length(status_line):
     """Check status length.
@@ -10,8 +13,9 @@ def check_status_length(status_line):
     >>> check_status_length("TOO_LONG" * 10)
     'Limit status to 50 characters'
     """
-    if len(status_line) > MAX_STATUS_LENGTH:
-        return "Limit status to {0} characters".format(MAX_STATUS_LENGTH)
+    if not _is_merge(status_line):
+        if len(status_line) > MAX_STATUS_LENGTH:
+            return "Limit status to {0} characters".format(MAX_STATUS_LENGTH)
 
 
 def check_status_formatting(status_line):
@@ -24,5 +28,9 @@ def check_status_formatting(status_line):
     >>> check_status_formatting("fix(th is): This is not good")
     'Conform status to `<type>(<scope>): <subject>` pattern'
     """
-    if STATUS_R.match(status_line) is None:
-        return "Conform status to `<type>(<scope>): <subject>` pattern"
+    errors = []
+    if not _is_merge(status_line):
+        if STATUS_R.match(status_line) is None:
+            errors.append(
+                "Conform status to `<type>(<scope>): <subject>` pattern")
+            return "\n".join(errors)

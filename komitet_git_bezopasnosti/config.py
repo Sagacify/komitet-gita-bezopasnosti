e488ea67046ae9eba31d4e491676d0f8bfdf7c00
dizballanze <dizballanze@gmail.com>
2016-05-31 16:26:40 +0300

    fix(config): cast numeric parameters to int

--- komitet_git_bezopasnosti/config.py
+++ komitet_git_bezopasnosti/config.py
@@ -5,8 +5,8 @@ import re
 HIDDEN = "<KGB>"
 STATUS_CONTEXT = "Комитет Git'a безопасности"
 
-MAX_STATUS_LENGTH = env.get("MAX_STATUS_LENGTH") or 50
-MAX_LINE_LENGTH = env.get("MAX_LINE_LENGTH") or 72
+MAX_STATUS_LENGTH = int(env.get("MAX_STATUS_LENGTH") or 50)
+MAX_LINE_LENGTH = int(env.get("MAX_LINE_LENGTH") or 72)
 
 _DEFAULT_TYPES = ",".join([
     "doc",

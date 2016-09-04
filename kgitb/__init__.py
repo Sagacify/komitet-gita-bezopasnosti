40faa874550aecef8950647e8aa3316d531a036b
Augustin Borsu <a.borsu@gmail.com>
2016-09-05 00:02:48 +0200

    refactor(deps): Move flask out of init

--- kgitb/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-from . import routes
-from . import config
-
-app = routes.setup(Flask(config.HIDDEN[1:-1]))
--- kgitb/__main__.py
+++ kgitb/__main__.py
@@ -1,10 +1,15 @@
 import logging
 import sys
 
-from .config import GH_TOKEN, HOST, PORT
-from . import app
+from flask import Flask
+
+from .config import GH_TOKEN, HOST, PORT, HIDDEN
+from . import routes
 
 log = logging.getLogger(__name__)
+
+
+app = routes.setup(Flask(HIDDEN[1:-1]))
 
 
 def main():

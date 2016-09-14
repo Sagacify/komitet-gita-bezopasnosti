cd834ef364bbb300ee1b350ef05097ad284865b5
Augustin Borsu <a.borsu@gmail.com>
2016-09-14 22:59:04 +0200

    version(0.1.0): Global un/install

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="kgitb",
     packages=["kgitb", "kgitb.rules"],
     scripts=["bin/resident"],
-    version="0.0.8",
+    version="0.1.0",
     description="A commit message linter",
     long_description=readme,
     author="Augustin Borsu",

c6655213c1b5dd0a965226a0c1fe9ed00f8a5580
Louis Borsu <sat@satprod.net>
2017-03-16 12:25:55 +0100

    feat(healthcheck): Add a healthcheck route

---
+++ kgitb/__about__.py
@@ -0,0 +1,20 @@
+# inspired from:
+# https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+
+__all__ = [
+    '__title__', '__summary__', '__uri__', '__version__',
+    '__author__', '__email__', '__license__', '__copyright__',
+]
+
+__title__ = 'komitet-gita-bezopasnosti'
+__summary__ = 'A commit message style enforcer.'
+__uri__ = 'https://github.com/Sagacify/komitet-gita-bezopasnosti'
+
+__version__ = '0.1.4'
+
+
+__author__ = 'Sagacify'
+__email__ = 'dev+python@sagacify.com'
+
+__license__ = 'GNU'
+__copyright__ = '2016 %s' % __author__
